c0c21378b83e156d00d546cb158695641cb781d1
dmm <dmm@dmm>
2023-03-05 22:50:28 -0600

    entrega final

--- Lab/Practicas/01/src/script.sh
+++ Lab/Practicas/01/src/script.sh
@@ -1,89 +1,61 @@
 #!/bin/bash
 
-## Dominios
-# unam.mx
-# pemex.com
-# gob.mx
-# ipn.mx
-# ips
-
-## Comandos:
-# implementados
-# Ping
-# nslookup
-#traceroute
-
-# pendientes
-#Nmap -O para sistema operativo, verificamos que es de oracle vm,
-#      -sS
-#      checar que es reverse dns
-
-#whois
-#sublist3r
-#subfinder
-#findomain
-#dnsmap
-#dnsrecon
-#EtherApe
-
 IFS=$'\n'
+
+function get_whois() {
+    cont=0
+    for i in $(whois -H $1); do	
+	if [ "${i:0:1}" != "%" ]; then	    
+	    echo "$i"
+	fi
+	cont=$(( cont + 1 ))
+    done
+}
 
 function get_nslookup() {
     cont=0
     for i in $(nslookup $1); do
 	if [ "$cont" -eq "3" ]; then
-	    echo "nombre: "${i:6}		
-	elif [ "$cont" -eq "4" ]; then
-	    echo "ipv4: "${i:9}
-	elif [ "$cont" -eq "6" ]; then
-	    echo "ipv6: "${i:9}
+	    echo "nombre: ${i:6}"		
+	elif [[ "${i:0:7}" == "Address" && "$cont" -ge "2" ]]; then
+	    echo "direcciòn: ${i:8}"	    
 	fi
 	cont=$(( cont + 1 ))
     done
 }
 
 function get_traceroute() {
-    echo -e "\t=====\n\tInfo de tracetoute \n\t====="
+    echo -e "\n\t=====\n\tSaltos para lograr la conexiòn \n\t====="
 
-    cont=0
     for i in $(traceroute $1); do
-	if [ "$cont" -eq "2" ]; then	    
-	    gateway=$i
-	fi
 	echo -e "\t${i}"
-	cont=$(( cont + 1 ))
     done
-    echo -e "\n\tEl gateway de la red es: ${gateway:14:11}\n"
-    echo -e "=====\n====="
+    
+    echo -e "\t=====\n\tFin de saltos\n\t====="
 }
 
 function get_nmap() {
-
+    echo -e "\t==="
     cont=0
     for i in $(nmap $1); do
-	if [ "$cont" -eq "2" ]; then
-	    latencia=$i$
-	    ## 3 para ips no checadas
-	    ## 4 para reverse dns
-	    ## 5 puertos no revisados
-	    ## >= 6 puerto estado servicio
+	if [ "$cont" -ge "2" ]; then
+	    echo -e "\t$i"
 	fi
 	cont=$(( cont + 1 ))
     done
-    echo -e "\tlatencia: ${latencia:12:5}"
+    echo -e "\t==="
 }
 
 ## start script
 ping -c1 $1 > /dev/null
 if [ ! $? -eq 0 ]; then
-    echo "No se pudo conectar a $1"
+    echo -e "error: no se pudo conectar a $1 con ping"
 else
     echo "=========="
-    echo "Se establecio conexión con $1"
+    echo "Se establecio conexión con $1 mediante ping"
     echo "=========="
-
-    get_nslookup "$1"
-    get_traceroute "$1"
-    get_nmap "$1"
-    
 fi
+get_nslookup "$1"
+get_traceroute "$1"
+get_nmap "$1"
+get_whois "$1"
